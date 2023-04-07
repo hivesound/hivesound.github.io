fdfe69f2384cec2789314393e57b9cb92e3ca5c6
kevinkrs <kraus.kevin@proton.me>
2023-04-07 16:48:23 +0200

    changed to vuejs3

---
+++ hivesound/src/layouts/default/HivesoundNavBar.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-app-bar flat>
+    <v-app-bar-title>
+      <router-link to="/">
+        <v-img
+          src="@/assets/HIVESOUND_CI_yellow.svg"
+          width="15rem">
+        </v-img>
+      </router-link>
+    </v-app-bar-title>
+  </v-app-bar>
+</template>
+
+<script lang="ts">
+  import { defineComponent } from "vue";
+
+  export default defineComponent({
+
+  })
+</script>
