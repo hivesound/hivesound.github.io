96224f318b4feaae6879d3d082ef3918be3d56f2
kevinkrs <kraus.kevin@proton.me>
2023-04-07 16:04:08 +0200

    changed framework to vuejs with ts and veutify3

---
+++ hivesound/src/components/HivesoundLanding.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-container class="fill-height">
+    <v-responsive class="d-flex align-center text-center fill-height">
+      <v-card
+        class="mb-4"
+        width="40%"
+        text="Site under construction. Stay tuned for more!"
+        variant="tonal"></v-card>
+      <v-img
+        contain
+        height="300"
+        src="@/assets/HIVESOUND_CI_waves.svg"
+      />
+    </v-responsive>
+  </v-container>
+</template>
+
+<script lang="ts">
+  import { defineComponent } from "vue";
+
+  export default defineComponent({
+    name: "HivesoundLandings"
+  })
+</script>
---
+++ hivesound/src/router/index.ts
@@ -0,0 +1,34 @@
+// Composables
+import { createRouter, createWebHistory } from 'vue-router'
+
+const routes = [
+  {
+    path: '/',
+    component: () => import('@/layouts/default/Default.vue'),
+    children: [
+      {
+        path: '',
+        name: 'Home',
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () => import(/* webpackChunkName: "home" */ '@/views/Home.vue'),
+      },
+      {
+        path: '/imprint',
+        name: 'Imprint',
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () => import(/* webpackChunkName: "home" */ '@/views/Imprint.vue'),
+      },
+    ],
+  },
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes,
+})
+
+export default router
