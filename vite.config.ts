03a4a514a046e4d6de2bb882a0775b1f1c581ff4
Julian Obrecht <julian.obrecht@pm.me>
2023-04-11 21:01:10 +0200

    Set base to project root

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/hivesound/',
+  base: '/',
   plugins: [
     vue({
       template: { transformAssetUrls }
