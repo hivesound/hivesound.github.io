85ef0fa17c7163c1d79293143cef4ab7e9a2f9dd
kevinkrs <kraus.kevin@proton.me>
2023-04-11 12:22:25 +0200

    code update from other repo

---
+++ hivesound/src/layouts/default/Default.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-app>
+    <hivesound-nav-bar />
+    <hivesound-view />
+    <hivesound-footer />
+  </v-app>
+</template>
+
+<script lang="ts">
+  import HivesoundNavBar from './HivesoundNavBar.vue'
+  import HivesoundView from './View.vue'
+  import HivesoundFooter from './HivesoundFooter.vue'
+  import { defineComponent } from 'vue'
+
+  export default defineComponent({
+    components: {
+      HivesoundNavBar,
+      HivesoundView,
+      HivesoundFooter
+    }
+  })
+</script>
