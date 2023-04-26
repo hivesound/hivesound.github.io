b522be484442ad09dde48d21ebd6e289b22badb4
kevinkrs <kraus.kevin@proton.me>
2023-04-26 19:46:41 +0200

    impact page extendet with what, why, how. Texting required

--- src/components/HivesoundImpact.vue
+++ src/components/HivesoundImpact.vue
@@ -1,9 +1,39 @@
 <template>
   <!-- TODO Create mainframe that is defined in default to not use those v-container classes -->
   <div class="main no-margin-x h-screen hivesound-waves" id="impact">
-    <v-row class="">
+    <v-row justify="start">
       <v-col cols="12" xxl="4" xl="4" lg="4" class="mx-16">
-         <h3 class="text-h3 mb-4">Our Idea...</h3>
+         <h3 class="text-h3 mb-4">What</h3>
+        <p class="text-subtitle-1">
+          Developing a platform for beekeepers and companies comitted to sustainability by keeping bees. Using AI driven
+          frequency analysis and a simple-to-use monitoring system, we derive valuable information about their bee-colonies
+          and the surrounding environment.
+        </p>
+        <p class="text-subtitle-1">
+          This way, we cannot only contribute to preservance of honey bees and biodiversity, but ultimately,
+          help ourselves to preserve a diverse and healthy nutrition, honey bee products and a colourful nature to
+          marvel at.
+        </p>
+      </v-col>
+    </v-row>
+    <v-row justify="end">
+      <v-col cols="12" xxl="4" xl="4" lg="4" class="mx-16 impact">
+         <h3 class="text-h3 mb-4">Why</h3>
+        <p class="text-subtitle-1">
+          Developing a platform for beekeepers and companies comitted to sustainability by keeping bees. Using AI driven
+          frequency analysis and a simple-to-use monitoring system, we derive valuable information about their bee-colonies
+          and the surrounding environment.
+        </p>
+        <p class="text-subtitle-1">
+          This way, we cannot only contribute to preservance of honey bees and biodiversity, but ultimately,
+          help ourselves to preserve a diverse and healthy nutrition, honey bee products and a colourful nature to
+          marvel at.
+        </p>
+      </v-col>
+    </v-row>
+    <v-row justify="start">
+      <v-col cols="12" xxl="4" xl="4" lg="4" class="mx-16 impact">
+         <h3 class="text-h3 mb-4">How</h3>
         <p class="text-subtitle-1">
           Developing a platform for beekeepers and companies comitted to sustainability by keeping bees. Using AI driven
           frequency analysis and a simple-to-use monitoring system, we derive valuable information about their bee-colonies
