deec8c780a03a8220b015c7b3040e1e8ae345956
Julian Obrecht <julian.obrecht@pm.me>
2023-07-13 13:54:59 +0200

    Add press kit page with our logo to website

--- src/router/routes.js
+++ src/router/routes.js
@@ -13,6 +13,14 @@ const routes = [
         // which is lazy-loaded when the route is visited.
         component: () => import(/* webpackChunkName: "home" */ 'pages/HivesoundImprint.vue'),
       },
+      {
+        path: '/press-kit',
+        name: 'Press Kit',
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () => import(/* webpackChunkName: "home" */ 'pages/HivesoundPressKit.vue'),
+      }
     ]
   },
 
