60ebf5c65a953094fb5bba0bd14e8c2d1edf5cd6
Julian Obrecht <julian.obrecht@pm.me>
2023-07-13 14:04:49 +0200

    Set default font color to white on press kit

--- src/pages/HivesoundPressKit.vue
+++ src/pages/HivesoundPressKit.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="">
-    <div class="q-mx-xl q-pb-md imprint">
+    <div class="q-mx-xl q-pb-md">
         <div class="overflow-auto">
             <h1>HIVESOUND Brand Identity Guide</h1>
             <div>
@@ -8,7 +8,7 @@
                     <h2 class="q-my-xl">Logo</h2>
                 </div>
                 <div>
-                    <p class="presskit-text">
+                    <p>
                         When mentioning HIVESOUND, please utilize our logo mark without any modifications. Our branding should not be used for your own open or closed source projects. Refer to our company as HIVESOUND only, in uppercase letters, and without any tagline below it.
                         The logo should be on a dark ground and should never be used on a light ground.
                     </p>
