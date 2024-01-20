60b3cd6e0bcf1a697317eef963fa8de8c5b39cd1
Julian Obrecht <julian.obrecht@pm.me>
2024-01-20 11:57:10 +0100

    Translated imprint

--- src/pages/HivesoundImprint.vue
+++ src/pages/HivesoundImprint.vue
@@ -2,12 +2,10 @@
   <div class="">
     <div class="q-mx-xl q-pb-md imprint">
         <div class="overflow-auto">
-            <h3>Imprint</h3>
+            <h3>{{ $t('pages.imprint.main_title') }}</h3>
             <div>
                 <div>
-                <h4 class="q-my-xl">
-                    Information according to &sect;5 TMG
-                </h4>
+                <h4 class="q-my-xl">{{ $t('pages.imprint.legal_information_title') }}</h4>
                 </div>
                 <div>
                     <p>
@@ -35,67 +33,16 @@
                 </div>
             </div>
             <div class="q-mt-xl">
-                <h4>
-                    Disclaimer of liability
-                </h4>
+                <h4>{{ $t('pages.imprint.disclaimer_of_liability_title') }}</h4>
 
-                <br />
+                <h5>{{ $t('pages.imprint.liability_for_links_title') }}</h5>
+                <p>{{ $t('pages.imprint.liability_for_links_text') }}</p>
 
-                <h5>
-                    Liability for links
-                    </h5>
-                <br />
-                <p>
-                    Our offer contains links to external third-party websites over
-                    whose content we have no influence. Therefore, we cannot accept
-                    any liability for this third-party content. The respective
-                    provider or operator of the pages is always responsible for the
-                    content of the linked pages. The linked pages were checked for
-                    possible legal violations at the time they were linked. No
-                    illegal content was found at the time the link was created. A
-                    permanent control of the content of the linked pages is not
-                    reasonable without concrete evidence of a violation of the law.
-                    As soon as we become aware of legal violations, we will remove
-                    such links immediately.
-                </p>
+                <h5>{{ $t('pages.imprint.liability_for_content_title') }}</h5>
+                <p>{{ $t('pages.imprint.liability_for_content_text') }}</p>
 
-                <br />
-                <h4>
-                    Liability for content
-                </h4>
-                <br />
-                <p>
-                    As a service provider, we are responsible for our own content on
-                    these pages in accordance with general law in accordance with
-                    Section 7 (1) of the German Telemedia Act (TMG). According to
-                    &sect;&sect; 8 to 10 TMG, as a service provider, we are not
-                    obliged to monitor transmitted or stored third-party information
-                    or to investigate circumstances that indicate illegal activity.
-                    Obligations to remove or block the use of information according
-                    to general laws remain unaffected. However, liability in this
-                    regard is only possible from the point in time at which we
-                    become aware of a specific legal violation. As soon as we become
-                    aware of such legal violations, we will remove this content
-                    immediately.
-                </p>
-                <br />
-                <h4>Copyright</h4>
-                <br />
-                <p>
-                    The content and works on these pages created by the website
-                    operator are subject to German copyright law. The duplication,
-                    processing, distribution and any kind of exploitation outside
-                    the limits of copyright law require the written consent of the
-                    respective author or creator. Downloads and copies of this
-                    website are only permitted for private, non-commercial use.
-                    Insofar as the content on this site was not created by the
-                    operator, the copyrights of third parties are observed. In
-                    particular contents of third parties are marked as such. If you
-                    should nevertheless become aware of a copyright infringement, we
-                    would ask you to notify us accordingly. As soon as we become
-                    aware of legal violations, we will remove such content
-                    immediately.
-                </p>
+                <h5>{{ $t('pages.imprint.copyright_title') }}</h5>
+                <p>{{ $t('pages.imprint.copyright_text') }}</p>
             </div>
         </div>
     </div>
