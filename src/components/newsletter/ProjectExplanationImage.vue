6a23e046b60782cf665064587c0fd6d8925dded9
Julian Obrecht <julian.obrecht@pm.me>
2024-01-22 22:10:09 +0100

    Add project explanation to newsletter

---
+++ src/components/newsletter/ProjectExplanationImage.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <q-img contain :src="image.img" style="max-width: 15rem;float: left; margin-right: 20px; margin-bottom: 20px;">
+    </q-img>
+    <br><br>
+    <p>{{ $t("components.newsletter.project.tab2.text") }}</p>
+  </div>
+</template>
+
+<script lang="ts">
+// TODO: Imports work fine, but still get syntax error highlighting
+import { defineComponent } from "vue"
+import product_image from "../../assets/hivesound_product_image_logo_centered.png"
+
+export default defineComponent({
+  data() {
+    return {
+      image: { img: product_image }
+    }
+  },
+})
+
+</script>
--- src/pages/HivesoundNewsletter.vue
+++ src/pages/HivesoundNewsletter.vue
@@ -1,22 +1,19 @@
 <template>
-  <div class="">
-    <div class="q-mx-xl q-pb-md">
-        <div class="overflow-auto">          
-          <hivesound-newsletter-banner-text/>
-          <br>
-          <hivesound-newsletter-form/>
-        </div>
-    </div>
-  </div>
+  <hivesound-newsletter-banner-text />
+  <br>
+  <hivesound-newsletter-form />
+  <br>
+  <hivesound-newsletter-project-explanation />
 </template>
 
 <script lang="ts">
 import HivesoundNewsletterForm from 'src/components/newsletter/HivesoundNewsletterForm.vue'
 import HivesoundNewsletterBannerText from 'src/components/newsletter/HivesoundNewsletterBannerText.vue'
+import HivesoundNewsletterProjectExplanation from 'src/components/newsletter/HivesoundNewsletterProjectExplanation.vue'
 import { defineComponent } from "vue";
 
 export default defineComponent({
-  components: { HivesoundNewsletterForm, HivesoundNewsletterBannerText },
-  name: 'IndexPage'  
+  components: { HivesoundNewsletterForm, HivesoundNewsletterBannerText, HivesoundNewsletterProjectExplanation },
+  name: 'IndexPage'
 })
 </script>
