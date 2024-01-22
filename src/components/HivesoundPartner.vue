3de3045f8b4d3223629b3e35af34d4fbeab90c09
Julian Obrecht <julian.obrecht@pm.me>
2024-01-22 22:20:19 +0100

    Added exist as partner

--- src/components/HivesoundPartner.vue
+++ src/components/HivesoundPartner.vue
@@ -7,6 +7,11 @@
           <q-img contain style="max-width: 16rem" src="~assets/ai_startup_hub.png" ></q-img>
         </a>
       </div>
+      <div class="col-12 col-md-6 col-lg-4 q-mb-md">
+        <a href="https://www.exist.de/" target="_blank">
+          <q-img contain style="max-width: 16rem" src="~assets/Logo-EXIST.jpg" ></q-img>
+        </a>
+      </div>
       <div class="col-12 col-md-6 col-lg-4">
         <a href="https://www.imkerei-finck.de/" target="_blank">
           <q-img contain style="max-width: 16rem" src="~assets/partner_imkerei_finck.png"></q-img>
