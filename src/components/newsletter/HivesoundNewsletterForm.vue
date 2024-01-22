6d37a030ddef9b5618f46ce07397eb1baccf55dd
Julian Obrecht <julian.obrecht@pm.me>
2024-01-22 22:13:36 +0100

    Improve iframe size on browser and mobile

--- src/components/newsletter/HivesoundNewsletterForm.vue
+++ src/components/newsletter/HivesoundNewsletterForm.vue
@@ -1,4 +1,16 @@
 <template>
-    <iframe class="cr_mail_responsive" :src="$t('components.newsletter.cleverreach.form_url')" width="100%" style="min-height: 300px; height: 70vh;" frameborder="0" scrolling="no">
+    <iframe class="cr_mail_responsive" :src="$t('components.newsletter.cleverreach.form_url')" width="100%" style="min-height: 300px; height: 50vh;" frameborder="0" scrolling="no">
     </iframe>
 </template>
+
+<style>
+
+/* BREAKPOINTS ----- */
+@media screen and (max-width: 767px) {
+	.cr_mail_responsive { height: 70vh;}
+}
+
+@media screen and (max-width: 480px) {
+	.cr_mail_responsive { height: 70vh; }
+}
+</style>
