6e6f1631595da0ea978e2fa1f8a4dc66b6863f31
Julian Obrecht <julian.obrecht@pm.me>
2024-01-22 22:15:42 +0100

    Hide not final text box

--- src/components/HivesoundImpact.vue
+++ src/components/HivesoundImpact.vue
@@ -14,12 +14,14 @@
       </div>
     </div>
 
+    <!--
     <div class="row justify-start  q-my-md">
       <div class="col-12 col-md-5 impact q-pa-md">
         <h3 class="">{{ $t('components.impact.box3_title') }}</h3>
         <p class="text-subtitle-1 q-mt-sm">{{ $t('components.impact.box3_text') }}</p>
       </div>
     </div>
+    -->
   </div>
 </template>
 
