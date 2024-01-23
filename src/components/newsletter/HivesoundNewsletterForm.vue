acf9beaefa615b254a4de199eb254f6f1bc99ab5
Julian Obrecht <neuhausjulian@users.noreply.github.com>
2024-01-23 10:32:03 +0100

    increase iframe height

--- src/components/newsletter/HivesoundNewsletterForm.vue
+++ src/components/newsletter/HivesoundNewsletterForm.vue
@@ -1,5 +1,5 @@
 <template>
-    <iframe class="cr_mail_responsive" :src="$t('components.newsletter.cleverreach.form_url')" width="100%" style="min-height: 300px; height: 50vh;" frameborder="0" scrolling="no">
+    <iframe class="cr_mail_responsive" :src="$t('components.newsletter.cleverreach.form_url')" width="100%" style="min-height: 300px; height: 60vh;" frameborder="0" scrolling="no">
     </iframe>
 </template>
 
@@ -7,10 +7,10 @@
 
 /* BREAKPOINTS ----- */
 @media screen and (max-width: 767px) {
-	.cr_mail_responsive { height: 70vh;}
+	.cr_mail_responsive { height: 100vh;}
 }
 
 @media screen and (max-width: 480px) {
-	.cr_mail_responsive { height: 70vh; }
+	.cr_mail_responsive { height: 100vh; }
 }
 </style>
