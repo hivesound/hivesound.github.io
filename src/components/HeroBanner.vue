fa9b4046bac23abd5790c30032395583bfb72835
Julian Obrecht <julian.obrecht@pm.me>
2024-01-24 22:31:55 +0100

    Add subscribe button to index page

--- src/components/HeroBanner.vue
+++ src/components/HeroBanner.vue
@@ -4,6 +4,9 @@
         contain
         src="~assets/HIVESOUND_CI_waves.png"
       />
+      <div class="text-center">
+        <a :href="$t('components.newsletter.cleverreach.form_url')" target="_blank" class="button-link">{{ $t('components.newsletter.button_label') }}</a>
+      </div>
     <div class="row arrow-icon">
       <div class="col text-center">
         <div class="q-my-xl q-pt-xl">
@@ -38,3 +41,26 @@ export default defineComponent({
 })
 
 </script>
+
+<style>
+.button-link {
+    display: inline-block;
+    padding: 0.5em 1em;
+    background-color: #FAA916; /* Example orange color */
+    color: black;
+    text-align: center;
+    text-decoration: none;
+    border-radius: 10px; /* Adjust for rounded corners */
+    font-weight: bold;
+    font-size: 1.5em; /* Adjust as needed */
+    border: none;
+    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2); /* Example shadow */
+    transition: background-color 0.3s, box-shadow 0.3s; /* Smooth transition for hover effect */
+}
+
+.button-link:hover {
+    background-color: #e69500; /* Darker orange on hover */
+    box-shadow: 0 6px 12px rgba(0, 0, 0, 0.3); /* Larger shadow on hover */
+    cursor: pointer;
+}
+</style>
--- src/i18n/en-US/components/newsletter.js
+++ src/i18n/en-US/components/newsletter.js
@@ -4,6 +4,8 @@ export default {
         text: "Subscribe to our HIVESOUND newsletter and become part of our exciting journey! In addition to the latest insights into our bee monitoring technology and digital tools for your beekeeping, a special opportunity awaits you: We will soon be looking for committed beekeepers who would like to test the platform together with us. Your experience and data will not only contribute to the improvement of HIVESOUND, but will also be part of our Open Data project, which aims to share knowledge and insights with everyone for the benefit of bees and the environment. Be at the forefront of revolutionizing the world of beekeeping and help us achieve something great!"
     },
 
+    button_label: "Subscribe to the newsletter",
+
     // newsletter-provider:
     cleverreach: {
         form_url: "https://seu2.cleverreach.com/f/375305-378133/"
