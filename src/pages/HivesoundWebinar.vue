635cae876679a5b2c3ef5e7725f4086b63d4e375
Julian Obrecht <julian.obrecht@pm.me>
2024-02-06 13:53:57 +0100

    Removed not required intro text

--- src/pages/HivesoundWebinar.vue
+++ src/pages/HivesoundWebinar.vue
@@ -1,6 +1,5 @@
 <template>
   <h1 class="primary-color" style="margin-bottom: 20px;">{{ $t('components.webinar.only-in-german') }}</h1>
-  <hivesound-webinar-banner-text />
   <br>
   <hivesound-webinar-form />
   <br>
@@ -12,12 +11,11 @@
 
 <script lang="ts">
 import HivesoundWebinarForm from 'src/components/webinar/HivesoundWebinarForm.vue'
-import HivesoundWebinarBannerText from 'src/components/webinar/HivesoundWebinarBannerText.vue'
 import HivesoundWebinarProjectExplanation from 'src/components/webinar/HivesoundWebinarProjectExplanation.vue'
 import { defineComponent } from "vue";
 
 export default defineComponent({
-  components: { HivesoundWebinarForm, HivesoundWebinarBannerText, HivesoundWebinarProjectExplanation },
+  components: { HivesoundWebinarForm, HivesoundWebinarProjectExplanation },
   name: 'IndexPage'
 })
 </script>
